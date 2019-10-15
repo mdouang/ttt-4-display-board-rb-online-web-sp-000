1982dc283ba433ad52b2926578c9686d49d97829
Maikaru <maikaru@maikaru.com>
2019-10-15 20:28:05 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -185,6 +185,16 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
+            board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | X | O ")
     end
   end
 end
